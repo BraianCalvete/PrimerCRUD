799178ea1659985422fdb697ce1f3146008c1da9
BraianCalvete <63986398+BraianCalvete@users.noreply.github.com>
2020-05-11 14:47:13 -0300

    Add files via upload

---
+++ veterinaria/veterinaria/Form1.Designer.vb
@@ -0,0 +1,184 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtColor = New System.Windows.Forms.TextBox()
+        Me.txtRaza = New System.Windows.Forms.TextBox()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.BRegistrar = New System.Windows.Forms.Button()
+        Me.BEditar = New System.Windows.Forms.Button()
+        Me.BEliminar = New System.Windows.Forms.Button()
+        Me.GrdVeterinaria = New System.Windows.Forms.DataGridView()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtId = New System.Windows.Forms.TextBox()
+        CType(Me.GrdVeterinaria, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'txtColor
+        '
+        Me.txtColor.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtColor.Location = New System.Drawing.Point(191, 213)
+        Me.txtColor.Name = "txtColor"
+        Me.txtColor.Size = New System.Drawing.Size(219, 26)
+        Me.txtColor.TabIndex = 17
+        '
+        'txtRaza
+        '
+        Me.txtRaza.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtRaza.Location = New System.Drawing.Point(191, 145)
+        Me.txtRaza.Name = "txtRaza"
+        Me.txtRaza.Size = New System.Drawing.Size(219, 26)
+        Me.txtRaza.TabIndex = 16
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtNombre.Location = New System.Drawing.Point(191, 80)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(219, 26)
+        Me.txtNombre.TabIndex = 15
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(50, 213)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(51, 20)
+        Me.Label7.TabIndex = 14
+        Me.Label7.Text = "Color"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(50, 145)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(51, 20)
+        Me.Label8.TabIndex = 13
+        Me.Label8.Text = "Raza"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(50, 80)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(71, 20)
+        Me.Label9.TabIndex = 12
+        Me.Label9.Text = "Nombre"
+        '
+        'BRegistrar
+        '
+        Me.BRegistrar.Location = New System.Drawing.Point(54, 265)
+        Me.BRegistrar.Name = "BRegistrar"
+        Me.BRegistrar.Size = New System.Drawing.Size(80, 32)
+        Me.BRegistrar.TabIndex = 18
+        Me.BRegistrar.Text = "REGISTRAR"
+        Me.BRegistrar.UseVisualStyleBackColor = True
+        '
+        'BEditar
+        '
+        Me.BEditar.Location = New System.Drawing.Point(191, 265)
+        Me.BEditar.Name = "BEditar"
+        Me.BEditar.Size = New System.Drawing.Size(80, 32)
+        Me.BEditar.TabIndex = 19
+        Me.BEditar.Text = "EDITAR"
+        Me.BEditar.UseVisualStyleBackColor = True
+        '
+        'BEliminar
+        '
+        Me.BEliminar.Location = New System.Drawing.Point(330, 265)
+        Me.BEliminar.Name = "BEliminar"
+        Me.BEliminar.Size = New System.Drawing.Size(80, 32)
+        Me.BEliminar.TabIndex = 20
+        Me.BEliminar.Text = "ELIMINAR"
+        Me.BEliminar.UseVisualStyleBackColor = True
+        '
+        'GrdVeterinaria
+        '
+        Me.GrdVeterinaria.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.GrdVeterinaria.Location = New System.Drawing.Point(462, 47)
+        Me.GrdVeterinaria.Name = "GrdVeterinaria"
+        Me.GrdVeterinaria.Size = New System.Drawing.Size(396, 250)
+        Me.GrdVeterinaria.TabIndex = 21
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(50, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(25, 20)
+        Me.Label1.TabIndex = 22
+        Me.Label1.Text = "Id"
+        '
+        'txtId
+        '
+        Me.txtId.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtId.Location = New System.Drawing.Point(191, 19)
+        Me.txtId.Name = "txtId"
+        Me.txtId.Size = New System.Drawing.Size(219, 26)
+        Me.txtId.TabIndex = 23
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(895, 341)
+        Me.Controls.Add(Me.txtId)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.GrdVeterinaria)
+        Me.Controls.Add(Me.BEliminar)
+        Me.Controls.Add(Me.BEditar)
+        Me.Controls.Add(Me.BRegistrar)
+        Me.Controls.Add(Me.txtColor)
+        Me.Controls.Add(Me.txtRaza)
+        Me.Controls.Add(Me.txtNombre)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label9)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.GrdVeterinaria, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtColor As TextBox
+    Friend WithEvents txtRaza As TextBox
+    Friend WithEvents txtNombre As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents BRegistrar As Button
+    Friend WithEvents BEditar As Button
+    Friend WithEvents BEliminar As Button
+    Friend WithEvents GrdVeterinaria As DataGridView
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtId As TextBox
+End Class
---
+++ veterinaria/veterinaria/Form1.vb
@@ -0,0 +1,130 @@
+﻿Imports MySql.Data.MySqlClient
+'Braian Calvete 3BF
+Public Class Form1
+
+    Dim conexion As MySqlConnection = New MySqlConnection
+    Dim cmd As New MySqlCommand
+    Sub ActualizarSelect()
+        Dim ds As DataSet = New DataSet
+        Dim adaptador As MySqlDataAdapter = New MySqlDataAdapter
+
+        conexion.ConnectionString = "server=localhost; database=veterinaria;Uid=root;Pwd=;"
+
+        Try
+
+            conexion.Open()
+            cmd.Connection = conexion
+
+            cmd.CommandText = "SELECT * FROM perros ORDER BY nombre "
+            adaptador.SelectCommand = cmd
+            adaptador.Fill(ds, "Tabla")
+            GrdVeterinaria.DataSource = ds
+            GrdVeterinaria.DataMember = "Tabla"
+
+            conexion.Close()
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+    Private Sub BRegistrar_Click(sender As Object, e As EventArgs) Handles BRegistrar.Click
+
+        conexion.ConnectionString = "server=localhost; database=veterinaria;Uid=root;Pwd=;"
+
+        Try
+
+            conexion.Open()
+            cmd.Connection = conexion
+
+            cmd.CommandText = "INSERT INTO perros(nombre,raza,color) VALUES (@nombre,@raza,@color)"
+            cmd.Prepare()
+
+            cmd.Parameters.Clear()
+            cmd.Parameters.AddWithValue("@nombre", txtNombre.Text)
+            cmd.Parameters.AddWithValue("@raza", txtRaza.Text)
+            cmd.Parameters.AddWithValue("@color", txtColor.Text)
+            cmd.ExecuteNonQuery()
+            txtNombre.Clear()
+            txtRaza.Clear()
+            txtColor.Clear()
+
+            conexion.Close()
+
+            ActualizarSelect()
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ActualizarSelect()
+    End Sub
+    Private Sub GrdVeterinaria_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles GrdVeterinaria.CellContentClick
+
+    End Sub
+
+    Private Sub GrdVeterinaria_CellContentClick(sender As Object, e As EventArgs) Handles GrdVeterinaria.SelectionChanged, GrdVeterinaria.CellContentClick
+        If (GrdVeterinaria.SelectedRows.Count > 0) Then
+            txtNombre.Text = GrdVeterinaria.Item("nombre", GrdVeterinaria.SelectedRows(0).Index).Value
+        End If
+    End Sub
+
+    Private Sub BEditar_Click(sender As Object, e As EventArgs) Handles BEditar.Click
+        conexion.ConnectionString = "server=localhost; database=veterinaria;Uid=root;Pwd=;"
+
+        Try
+
+            conexion.Open()
+            cmd.Connection = conexion
+
+            cmd.CommandText = "UPDATE perros Set nombre=@nombre, raza=@raza, color=@color WHERE id=@id"
+            cmd.Prepare()
+
+            cmd.Parameters.Clear()
+            cmd.Parameters.AddWithValue("@nombre", txtNombre.Text)
+            cmd.Parameters.AddWithValue("@raza", txtRaza.Text)
+            cmd.Parameters.AddWithValue("@color", txtColor.Text)
+            cmd.Parameters.AddWithValue("@id", txtId.Text)
+            cmd.ExecuteNonQuery()
+            txtNombre.Clear()
+            txtRaza.Clear()
+            txtColor.Clear()
+            txtId.Clear()
+
+            conexion.Close()
+
+            ActualizarSelect()
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+    Private Sub BEliminar_Click(sender As Object, e As EventArgs) Handles BEliminar.Click
+        conexion.ConnectionString = "server=localhost; database=veterinaria;Uid=root;Pwd=;"
+
+        Try
+
+            conexion.Open()
+            cmd.Connection = conexion
+
+            cmd.CommandText = "DELETE FROM perros WHERE id=@id"
+            cmd.Prepare()
+
+            cmd.Parameters.Clear()
+            cmd.Parameters.AddWithValue("@nombre", txtNombre.Text)
+            cmd.Parameters.AddWithValue("@raza", txtRaza.Text)
+            cmd.Parameters.AddWithValue("@color", txtColor.Text)
+            cmd.Parameters.AddWithValue("@id", txtID.Text)
+            cmd.ExecuteNonQuery()
+            txtNombre.Clear()
+            txtRaza.Clear()
+            txtColor.Clear()
+            txtId.Clear()
+
+            conexion.Close()
+
+            ActualizarSelect()
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+End Class
